3ffe1e8563a3a0ffe5f971272f4f23066e667d85
Julien Huel <jhuel@users.noreply.github.com>
2019-11-01 17:10:01 -0400

    Update roll.py

--- plugins/roll.py
+++ plugins/roll.py
@@ -1,5 +1,6 @@
 import re
 import requests
+import datetime
 
 from random import randint
 from mmpy_bot.bot import listen_to
@@ -8,6 +9,22 @@ from mmpy_bot.bot import respond_to
 @respond_to('^roll ?(\d+)?$', re.IGNORECASE)
 def roll_number(message, number=100):
     number = 100 if number is None else number
+    
+    if number is 420 :
+        
+        now = datetime.datetime.now()
+        
+        if (now.hour is 4 or now.hour is 16) and now.minute is 20 :
+            
+            random = randint(1,int(number))
+            
+            if random is 420:
+                message.reply('###BIG WINNER WOW ' + random)
+            else:
+                message.reply(random + ' :chuckle:')
+        else:
+            message.reply('Spa leur smh')
+        
     if int(number) > 1:
         random = randint(1,int(number))
         message.reply(random)
