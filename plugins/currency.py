c38828965e4dc74aa888fbfe0349844955e74cbc
Christian Artin <gravufo@gmail.com>
2019-11-01 21:25:59 -0400

    Add default values to currency conversion

--- plugins/currency.py
+++ plugins/currency.py
@@ -4,8 +4,12 @@ import requests
 from mmpy_bot.bot import listen_to
 from mmpy_bot.bot import respond_to
 
-@respond_to('^convert (\d+) ?(\w{3}) to (\w{3})$', re.IGNORECASE)
-def rates(message, amount, baseCurrency, targetCurrency):
+@respond_to('^convert$', re.IGNORECASE)
+@respond_to('^convert (\d+)? ?(\w{3}) (?:to )?(\w{3})$', re.IGNORECASE)
+def rates(message, amount=None, baseCurrency=None, targetCurrency=None):
+        amount = '1' if amount is None else amount
+        baseCurrency = 'CAD' if baseCurrency is None else baseCurrency
+        targetCurrency = 'USD' if targetCurrency is None else targetCurrency
         url = 'https://frankfurter.app/latest?from={baseCurrency}&to={targetCurrency}&amount={amount}'.format(baseCurrency=baseCurrency, targetCurrency=targetCurrency, amount=amount)
         r = requests.get(url)
         if r.status_code == 200:
