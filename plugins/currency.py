7dc5beb6c7988a3580cb76dd2797f432aa766743
Christian Artin <gravufo@gmail.com>
2019-11-01 21:09:25 -0400

    Various fixes and updates

--- plugins/currency.py
+++ plugins/currency.py
@@ -4,19 +4,15 @@ import requests
 from mmpy_bot.bot import listen_to
 from mmpy_bot.bot import respond_to
 
-@respond_to('currency (\w{3}) (\w{3})$', re.IGNORECASE)
-def rates(message, base, otherCurrency):
-        url = 'http://api.fixer.io/latest?base={base}'.format(base=base)
+@respond_to('^convert (\d+) ?(\w{3}) to (\w{3})$', re.IGNORECASE)
+def rates(message, amount, baseCurrency, targetCurrency):
+        url = 'https://frankfurter.app/latest?from={baseCurrency}&to={targetCurrency}&amount={amount}'.format(baseCurrency=baseCurrency, targetCurrency=targetCurrency, amount=amount)
         r = requests.get(url)
-        if r.status_code == 422:
-            message.reply('Invalid base :huel:')
-        elif r.status_code == 200:
+        if r.status_code == 200:
             jsonResponse = r.json()
-            if otherCurrency.upper() in jsonResponse['rates']:
-                # print(jsonResponse['rates'][otherCurrency.upper()])
-                rate = jsonResponse['rates'][otherCurrency.upper()] 
-                message.reply('Conversion rate for {base} to {currency} is: {rate}'.format(base=base.upper(), currency=otherCurrency.upper(), rate=rate))
+            if targetCurrency.upper() in jsonResponse['rates']:
+                message.reply('{amount} {baseCurrency} = {convertedAmount} {targetCurrency}'.format(amount=amount, baseCurrency=baseCurrency.upper(), convertedAmount=jsonResponse['rates'][targetCurrency.upper()], targetCurrency=targetCurrency.upper()))
             else:
-                print('NOT')
+                print('Error converting currency.')
 
 rates.__doc__ = 'Displays conversion rate between two currencies'
--- plugins/roll.py
+++ plugins/roll.py
@@ -7,29 +7,28 @@ from mmpy_bot.bot import listen_to
 from mmpy_bot.bot import respond_to
 
 @respond_to('^roll ?(\d+)?$', re.IGNORECASE)
-def roll_number(message, number=100):
-    
-    if number is 420 :
-        
+def roll_number(message, number):
+
+    number = 100 if number is None else int(number)
+
+    if number == 420 :
+
         now = datetime.datetime.now()
-        
-        if (now.hour is 4 or now.hour is 16) and now.minute is 20 :
-            
-            random = randint(1,int(number))
-            
-            if random is 420:
-                message.reply('###BIG WINNER WOW ' + random)
+
+        if (now.hour == 4 or now.hour == 16) and now.minute == 20 :
+
+            random = randint(1, number)
+
+            if random == 420:
+                message.reply('### BIG WINNER WOW ' + random)
             else:
                 message.reply(random + ' :chuckle:')
         else:
             message.reply('Spa leur smh')
-    else :
-        number = 100 if number is None else number
-        
-        if int(number) > 1:
-            random = randint(1,int(number))
-            message.reply(random)
-        else:
-            message.reply(number + ' is not a valid number for roll command')
+    elif number > 1:
+        random = randint(1, number)
+        message.reply(random)
+    else:
+        message.reply(number + ' is not a valid number for roll command')
 
 roll_number.__doc__ = "Roll between 1 and {{number}}, between 1 and 100 if nothing specified"
