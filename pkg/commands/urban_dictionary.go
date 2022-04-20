2fad5035e5fc4e0fd83ace71f1fbd7a2c695b800
Christian Artin <gravufo@gmail.com>
2022-04-19 23:17:44 -0400

    fix error handling in urban command

--- pkg/commands/urban_dictionary.go
+++ pkg/commands/urban_dictionary.go
@@ -1,12 +1,18 @@
 package commands
 
 import (
+	"errors"
+
 	ud "github.com/dpatrie/urbandictionary"
 )
 
 func GetUrbanDictionaryDefinition(term string) (def *ud.Result, err error) {
 	res, err := ud.Query(term)
 	if err != nil {
+		return
+	}
+	if len(res.Results) == 0 {
+		err = errors.New("No results found")
 		return
 	}
 
